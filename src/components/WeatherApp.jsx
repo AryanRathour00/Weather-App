4640241084f2164b963f07c432afc6aef30851b4
= <=>
2024-07-14 03:14:08 +0530

    finish the design of weather app

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -8,20 +8,20 @@ const WeatherApp = () => {
     <>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-3 border border-5 border-dark weather-sidebar">
+          <div className="col-3 weather-sidebar p-5">
             <div className="row">
-              <div className="col-12">
-                <div className="input-group my-5">
-                  <input type="search" className="form-control" />
-                  <button className="btn">
+              <div className="col-12 mt-2">
+                <div className="input-group mb-5">
+                  <input type="search" className="form-control" placeholder="Search" />
+                  <button className="btn btn-light">
                     <FaSearch />
                   </button>
                 </div>
               </div>
-              <div className="col-12">
+              <div className="col-12 d-flex justify-content-center mb-3">
                 <img src={cloudyImg} alt="" className="image-fluid weather-img" />
               </div>
-              <div className="col-12">
+              <div className="col-12 my-5">
                 <span className="d-flex">
                   <h1>6</h1>
                   <h4>°C</h4>
@@ -35,89 +35,89 @@ const WeatherApp = () => {
               </div>
             </div>
           </div>
-          <div className="col-9 border border-5 border-primary">
+          <div className="col-9 weather-description">
             <div className="row">
-              <div className="col-12">
-                <h1>Today's Highlights</h1>
+              <div className="col-12 px-5 mt-5 mb-4">
+                <h1 className="fs-4">Today's Highlights</h1>
               </div>
-              <div className="col-12 px-5 card-container">
+              <div className="col-12 px-2 card-container mb-4">
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1 ">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1 ">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1 ">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1 ">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
                 <div className="card weather-desc-card">
                   <div className="card-body text-center">
-                    <p>Humidity</p>
-                    <span>
+                    <p className='mb-0'>Humidity</p>
+                    <span className="fs-1 ">
                       <LuWaves />
                     </span>
-                    <p>
+                    <p className="mb-0 mt-2">
                       89<span>%</span>
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="col-12">
-                <h1>This Week</h1>
+              <div className="col-12 px-5 mb-4">
+                <h1 className="fs-4">This Week</h1>
               </div>
-              <div className="col-12 px-5 card-container">
+              <div className="col-12 px-2 card-container mb-5">
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -128,11 +128,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -143,11 +143,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -158,11 +158,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -173,11 +173,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -188,11 +188,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -203,11 +203,11 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
                       <li>
                         <p>8°C</p>
                       </li>
@@ -218,12 +218,12 @@ const WeatherApp = () => {
                   </div>
                 </div>
                 <div className="card week-desc-card">
-                  <div className="card-body">
+                  <div className="card-body text-center">
                     <p>Wednesday, April 3</p>
                     <img src={cloudyImg} alt="" width={50} />
                     <p>heavy intensity rain</p>
-                    <ul className="d-flex gap-3 mb-0">
-                      <li>
+                    <ul className="d-flex gap-3 mb-0 justify-content-center ps-0">
+                      <li> 
                         <p>8°C</p>
                       </li>
                       <li>
